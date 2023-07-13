867dd1910a3b92ce026a61c27b5ae0a4270ee9d1
w-e-w <40751091+w-e-w@users.noreply.github.com>
2023-07-13 18:33:32 +0900

    Update build_index.py

--- build_index.py
+++ build_index.py
@@ -1,15 +1,6 @@
 from pathlib import Path
 import json
 import datetime
-import subprocess
-import validate_json
-
-
-def run_command(command: list):
-    result = subprocess.run(command, capture_output=True, text=True)
-    assert result.returncode == 0, f'{["git", "switch", "master"]} {result.returncode}: {result.stderr}'
-    return result
-
 
 def read_extension(file: Path):
     with open(file, 'r') as f:
@@ -34,13 +25,26 @@ def read_extension(file: Path):
             json.dump(extension, f, indent=4)
     return extension
 
+
+def update_index(index_path: str, exts: dict, tags: dict):
+    with open(index_path, 'r') as f:
+        existing_extensions = {extension['url']: extension for extension in json.load(f)['extensions']}
+
+    # update existing remove removed and add new extensions
+    for extensions_url, extension in exts.items():
+        if extensions_url in existing_extensions.keys():
+            existing_extensions[extensions_url].update(extension)
+        else:
+            existing_extensions[extensions_url] = extension
+    extensions_list = [extension for extensions_url, extension in existing_extensions.items() if extensions_url in extensions]
+    extension_index = {'tags': tags, 'extensions': extensions_list}
+
+    with open(index_path, 'w') as f:
+        json.dump(extension_index, f, indent=4)
+    return extension_index
+
+
 if __name__ == "__main__":
-
-    run_command(["git", "config", "user.name", "'github-actions[bot]'"])
-    run_command(["git", "config", "user.email", "'github-actions[bot]@users.noreply.github.com'"])
-
-    # switch to extensions branch
-    run_command(["git", "switch", "extensions"])
 
     with open('tags.json', 'r') as f:
         tags = json.load(f)
@@ -53,33 +57,8 @@ if __name__ == "__main__":
             extension = read_extension(f)
             extensions[extension['url']] = extension
 
-    if run_command(["git", "status", "--short"]).stdout:
-        validate_json.validate()
-        run_command(["git", "commit", "--amend", "--no-edit", "--all"])
-        run_command(["git", "push", "--force"])
-
     # switch to extensions branch
-    run_command(["git", "fetch", "origin", "master", "--depth", "1"])
-    run_command(["git", "switch", "master"])
 
-    with open('index.json', 'r') as f:
-        existing_extensions = {extension['url']: extension for extension in json.load(f)['extensions']}
-
-    # update existing remove removed and add new extensions
-    for extensions_url, extension in extensions.items():
-        if extensions_url in existing_extensions.keys():
-            existing_extensions[extensions_url].update(extension)
-        else:
-            existing_extensions[extensions_url] = extension
-    extensions_list = [extension for extensions_url, extension in existing_extensions.items() if extensions_url in extensions]
-    extension_index = {'tags': tags, 'extensions': extensions_list}
-
-    with open('index.json', 'w') as f:
-        json.dump(extension_index, f, indent=4)
-
-    print(f'{len(tags)} tags, {len(extensions_list)} extensions')
-
-    if run_command(["git", "status", "--short"]).stdout:
-        validate_json.validate()
-        run_command(["git", "commit", "--amend", "--no-edit", "--all"])
-        run_command(["git", "push", "--force"])
+    update_index('index.json', extensions, tags)
+    extension_index = update_index('../master/index.json', extensions, tags)
+    print(f'{len(tags)} tags, {len(extension_index.get("extensions_list"))} extensions')
